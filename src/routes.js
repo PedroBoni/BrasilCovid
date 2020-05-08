0538a6f78254defbafcf59a4e772910b9580c4ef
PedroBoni <pedro.henrique1210@outlook.com>
2020-05-07 22:32:15 -0300

    config Homepage

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/Home" exact component={Home}/>
         </Switch>
     </BrowserRouter>
   );
